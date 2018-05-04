1d8f0d075e10a9fcaa78e927a4140cc1d80d2d9c
fuyingdi <fuyingdi@126.com>
2018-05-04 22:41:35 +0800

    '添加了解析yaml'

---
+++ crawl_learningopengl.py
@@ -0,0 +1,11 @@
+from element_screenshot import shot_element
+from print_to_pdf import combine_to_pdf
+import yaml
+
+with open('target.yaml', "r") as f:
+    target = yaml.load(f)
+# print(target['target']
+for target in target['target']:
+    shot_element(target['url'], target['element_name'])
+
+combine_to_pdf('image')
